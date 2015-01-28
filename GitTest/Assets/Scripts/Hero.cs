b6c74ee2747d4004cf53ca85c721178ef277a8f4
test <test@qq.com>
2015-01-28 18:12:24 +0800

    no message

---
+++ GitTest/Assets/Scripts/Hero.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	Debug.Log("hero complate");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
