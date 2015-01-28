57d65a6a46952233304c125141d8ffb58ffadd55
zhangyanlei <zyl9857@sina.com>
2015-01-28 17:57:13 +0800

    1

---
+++ GitTest/Assets/GitTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("This is test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
