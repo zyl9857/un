67af6b313607307835cb26b3281a50c606980839
test <test@qq.com>
2015-01-28 18:06:48 +0800

    ^^^

--- GitTest/Assets/Scripts/GameTest.cs
+++ GitTest/Assets/Scripts/GameTest.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class GameTest : MonoBehaviour {
-
+	
+	void Awake()
+	{
+		Debug.Log("awake");
+	}
 	// Use this for initialization
 	void Start () {
 	Debug.Log("test");
