03b0477abe3e81eb9e864ba31c79af14613d1786
zhangyanlei <zyl9857@sina.com>
2015-01-29 11:04:46 +0800

    1.this

--- GitTest/Assets/Scripts/Hero.cs
+++ GitTest/Assets/Scripts/Hero.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class Hero : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	Debug.Log("hero complate");
+	
+	Debug.Log("This is wrong");
 	}
 	
 	// Update is called once per frame
